c0dd8392b889d46c0337653cb50dfd9f9063d2c0
Jakub Giminski <jakubgiminski@gmail.com>
2017-11-28 12:09:05 +0000

    Adds a test for GetAccesses

---
+++ tests/Commands/GetAccessesTest.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace FeatureKeys\Tests\Commands;
+
+use FeatureKeys\Commands\GetAccesses\GetAccessesCommand;
+use FeatureKeys\Commands\GetAccesses\GetAccessesCommandHandler;
+use FeatureKeys\FeatureAccess\FeatureAccessContainerFactory;
+use FeatureKeys\FeatureAccess\FeatureAccessRepository;
+use FeatureKeys\Tests\StarWars\FeatureAccessConfig;
+use FeatureKeys\Tests\StarWars\FeatureAccessOverrideConfig;
+use FeatureKeys\Tests\StarWars\FeatureOverride\FeatureAccessOverrideContainerFactory;
+use FeatureKeys\Tests\StarWars\FeatureOverride\FeatureAccessOverrideFactory;
+use FeatureKeys\Tests\StarWars\FeatureOverride\Parameters\CountryId;
+use FeatureKeys\Tests\StarWars\FeatureOverride\Parameters\PlayerId;
+use FeatureKeys\Tests\StarWars\FeatureOverride\Parameters\TeamId;
+use FeatureKeys\Tests\StarWars\FeatureOverride\Parameters\UniverseId;
+use PHPUnit\Framework\TestCase;
+
+class GetAccessesTest extends TestCase
+{
+    private $overrides;
+
+    private $repository;
+
+    private $accesses;
+
+    public function setUp(): void
+    {
+        $overridesFactory = new FeatureAccessOverrideContainerFactory(
+            new FeatureAccessOverrideFactory(
+                new PlayerId('darkslayer097'),
+                new TeamId('GoVirtusPro'),
+                new UniverseId('Tatooine'),
+                new CountryId('polska')
+            )
+        );
+        $this->overrides = $overridesFactory->createFromConfig(new FeatureAccessOverrideConfig());
+        $accessesFactory = new FeatureAccessContainerFactory();
+        $this->accesses = $accessesFactory(new FeatureAccessConfig());
+        $this->repository = \Mockery::mock(FeatureAccessRepository::class)
+            ->shouldReceive('getForOverrides')
+            ->andReturn($this->accesses)
+            ->getMock();
+    }
+
+    public function testCanGetAccesses(): void
+    {
+        $command = new GetAccessesCommand($this->overrides);
+        $commandHandler = new GetAccessesCommandHandler($this->repository);
+        $response = $commandHandler($command);
+        self::assertSame($this->accesses, $response->getAccesses());
+    }
+}
