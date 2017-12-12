ecf5762f2af0c3288bfe3a0bf492003b3e0070b7
Jakub Giminski <jakubgiminski@gmail.com>
2017-12-12 12:57:24 +0000

    Introduces FeatureOverrideType value object

---
+++ src/FeatureOverride/FeatureOverrideType.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace FeatureKeys\FeatureOverride;
+
+final class FeatureOverrideType
+{
+    private $value;
+
+    private function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public static function access(): self
+    {
+        return new self('ACCESS');
+    }
+
+    public static function value(): self
+    {
+        return new self('VALUE');
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
