892002bfdaafae8edd73b67053c461764fb9c45f
Jakub Giminski <jakubgiminski@gmail.com>
2018-01-11 10:25:19 +0000

    Implements Feature Value Types

--- src/FeatureValue/FeatureValue.php
+++ src/FeatureValue/FeatureValue.php
@@ -7,9 +7,17 @@ abstract class FeatureValue
 {
     protected $value;
 
-    protected function __construct($value)
+    private $type;
+
+    protected function __construct($value, ValueType $type)
     {
         $this->value = $value;
+        $this->type = $type;
+    }
+
+    public function getType(): ValueType
+    {
+        return $this->type;
     }
 
     abstract public static function getName(): string;
